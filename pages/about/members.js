04b6f6f25bbf37801138cedaaef060e0143431fd
gsofter <real.kingstar@outlook.com>
2021-01-22 07:06:16 -0600

    update members page

--- pages/about/members.js
+++ pages/about/members.js
@@ -1,35 +1,18 @@
 import LeaderMember from '../../components/LeaderMember';
+import { leaders, members } from './data';
 
-const leaders = [
-  {
-    photoSrc: `${__dirname}images/members/manindra_ma_jumar.png`,
-    firstName: 'manindra',
-    lastName: 'ma jumar',
-    role: 'Chief Executive Officer',
-    description:
-      'As an experienced entrepreneur with a penchant for new technology, Manindra has worked in international business for over 7 years in 15+ countries. Experienced in AI, AR & Blockchain, he has launched several products in his entrepreneurial and professional career and continues to inspire and innovate as Founder & CEO at Tokenizer. He holds an MBA from the Hult International Business School (San Francisco, USA) Bachelor’s in Business Administration from the Edinburgh Napier University (Scotland, UK).',
-  },
-  {
-    photoSrc: `${__dirname}images/members/manindra_ma_jumar.png`,
-    firstName: 'manindra',
-    lastName: 'ma jumar',
-    role: 'Chief Executive Officer',
-    description:
-      'As an experienced entrepreneur with a penchant for new technology, Manindra has worked in international business for over 7 years in 15+ countries. Experienced in AI, AR & Blockchain, he has launched several products in his entrepreneurial and professional career and continues to inspire and innovate as Founder & CEO at Tokenizer. He holds an MBA from the Hult International Business School (San Francisco, USA) Bachelor’s in Business Administration from the Edinburgh Napier University (Scotland, UK).',
-  },
-];
-export default function AboutPage() {
+export default function MembersPage() {
   return (
     <>
       {/* Begin Members Sections */}
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center mt-12 mb-12">
         {/* Leaders Section */}
         <div className="flex flex-col justify-center bg-white container py-10">
           <h3 className="text-3xl md:text-4xl font-bold text-primary">
             Leadership
           </h3>
           <hr className="border-secondary border-t-4 my-8" />
-          <div className="leaders grid grid-cols-2 md:grid-cols-4 gap-4">
+          <div className="leaders grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 md:gap-x-4 gap-y-4 md:gap-y-12">
             {leaders.map(({ photoSrc, firstName, lastName, role }, index) => (
               <LeaderMember
                 key={index}
@@ -40,6 +23,37 @@ export default function AboutPage() {
               />
             ))}
           </div>
+        </div>
+        {/* Leaders Section */}
+        <div className="flex flex-col justify-center bg-white container py-10">
+          <h3 className="text-3xl md:text-4xl font-bold text-primary">TEAM</h3>
+          <hr className="border-secondary border-t-4 my-8" />
+          <div className="leaders grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
+            {members.map(({ photoSrc, firstName, lastName, role }, index) => (
+              <LeaderMember
+                key={index}
+                photoSrc={photoSrc}
+                firstName={firstName}
+                lastName={lastName}
+                role={role}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* Begin Join Us Section */}
+      <div className="flex flex-col items-center justify-center bg-home-3 bg-no-repeat bg-cover mt-32 py-10 md:py-20 px-6">
+        <div className="flex flex-col items-center justify-center lg:container text-white py-160 lg:max-w-914">
+          <h3 className="text-3xl md:text-4xl font-bold text-white text-center">
+            Join Us
+          </h3>
+          <p className="text-center text-xl mt-4 md:mt-6">
+            We’re always looking for amazing people to join our growing team
+          </p>
+          <button className="btn-primary font-medium text-primary bg-white text-xl w-full md:w-auto mt-4 md:mt-12">
+            Careers
+          </button>
         </div>
       </div>
     </>
